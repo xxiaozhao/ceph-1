e74f7c0f776c83a679209f1b8caf7f44334a9a65
Alexander Indenbaum <aindenba@redhat.com>
2024-01-19 08:52:45 +0000

    tweak commit nvmeof ha demo
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

--- src/test/test_nvmeof_ha_demo.sh
+++ src/test/test_nvmeof_ha_demo.sh
@@ -1,3 +1,6 @@
+pushd ../src/nvmeof/gateway
+make down
+popd
 ../src/stop.sh
 ../src/vstart.sh --new --without-dashboard --memstore
 ./bin/ceph osd pool create rbd 
@@ -5,14 +8,31 @@
 ./bin/rbd -p rbd create demo_image1 --size 10M
 ./bin/rbd -p rbd create demo_image2 --size 10M
 pushd ../src/nvmeof/gateway
-make down
 docker-compose up -d --scale nvmeof=2 nvmeof
-sleep 10
+for i in $(seq 2); do
+    while true; do
+        sleep 1  # Adjust the sleep duration as needed
+        GW_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /'$i'/ {print $1}')
+        container_status=$(docker inspect -f '{{.State.Status}}' "$GW_NAME")
+        if [ "$container_status" == "running" ]; then
+           echo "Container $i $GW_NAME is now running."
+        else
+           echo "Container $i $GW_NAME is still not running. Waiting..."
+	   continue
+        fi
+        GW_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$GW_NAME")"
+        if docker-compose run --rm nvmeof-cli --server-address $GW_IP --server-port 5500 get_subsystems 2>&1 | grep -i failed; then
+           echo "Container $i $GW_NAME $GW_IP no subsystems. Waiting..."
+	   continue
+	fi
+        echo "Container $i $GW_NAME $GW_IP subsystems:"
+        docker-compose run --rm nvmeof-cli --server-address $GW_IP --server-port 5500 get_subsystems
+	break;
+    done
+done
 docker ps
-GW1=ceph-nvmeof_nvmeof_1
-GW2=ceph-nvmeof_nvmeof_2
-GW1_NAME=$(docker ps -aqf "name=${GW1}")
-GW2_NAME=$(docker ps -aqf "name=${GW2}")
+GW1_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /1/ {print $1}')
+GW2_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /2/ {print $1}')
 GW1_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$GW1_NAME")"
 GW2_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$GW2_NAME")"
 
